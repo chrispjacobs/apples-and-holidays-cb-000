65aec2fb314f9e2e733203127f8a20b2bb28705f
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-08 16:54:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,8 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
   holiday.each do |holiday, supplies|
-    final_holiday = holiday.to_s.tr("_", " ").capitalize
+    change_1 = holiday.to_s.tr("_", " ")
+    final_holiday = change_1.split.map(&:capitalize).join(" ")
     print "#{final_holiday}: "
     total_supplies = supplies.count
     counter = 1
